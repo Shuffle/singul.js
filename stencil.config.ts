ef72c05700da2db4ccae6d5d88e46a0fe1b932b6
monilprajapati <monillprajapatii@gmail.com>
2025-06-17 16:34:08 +0530

    package change

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'auth-search',
+  namespace: 'app-auth-search',
   outputTargets: [
     {
       type: 'dist',
